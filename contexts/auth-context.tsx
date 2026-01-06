ad6ac61eb271f22d737e49f10bf950ded2dcc5a0
kidus-collab <guyfreaky894@gmail.com>
2026-01-06 10:01:56 -0500

    3rd commit

--- contexts/auth-context.tsx
+++ contexts/auth-context.tsx
@@ -80,6 +80,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       email,
       password,
       options: {
+        emailRedirectTo: `${window.location.origin}/auth/callback`,
         data: {
           full_name: name,
         },
